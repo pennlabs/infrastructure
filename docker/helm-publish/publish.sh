588b498cb361a2b45e5f037a4a1408658e86ebda
Peyton Walters <pwalters18@bloomberg.net>
2020-01-04 22:19:00 -0500

    Add helm publish

---
+++ docker/helm-publish/publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+helm package icarus --destination .deploy
+cr upload -o pennlabs -r helm-charts -p .deploy
+git checkout gh-pages
+cr index -i index.yaml -p .deploy -o pennlabs -r helm-charts --charts-repo https://github.com/pennlabs/helm-charts
+git add index.yaml
+git commit -m "release new version"
+git push
+git checkout master
+rm -rf .deploy
+
