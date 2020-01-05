3ab5cbd2fad255fa1d6cb1d86712f7984f912b1c
Peyton Walters <pwalters18@bloomberg.net>
2020-01-05 11:32:17 -0500

    ci skip

--- docker/helm-publish/publish.sh
+++ docker/helm-publish/publish.sh
@@ -7,7 +7,7 @@ cr upload -o pennlabs -r helm-charts -p .deploy
 git checkout gh-pages
 cr index -i index.yaml -p .deploy -o pennlabs -r helm-charts --charts-repo https://github.com/pennlabs/helm-charts
 git add index.yaml
-git commit -m "release new version"
+git commit -m "release new version [ci skip]"
 git push
 git checkout master
 rm -rf .deploy
