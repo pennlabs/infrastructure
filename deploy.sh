79e3f4748801ebbe53b9bba66c28c66a06ae4e15
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2020-01-10 23:53:55 -0500

    Fix version tags

--- deploy.sh
+++ deploy.sh
@@ -26,4 +26,4 @@ fi
 
 helm repo add pennlabs https://helm.pennlabs.org/
 
-helm upgrade $RELEASE_NAME  --install --set=image_tag=$IMAGE_TAG -f k8s/values.yaml --version --version "${DEPLOY_TAG}" pennlabs/icarus
+helm upgrade $RELEASE_NAME  --install --set=image_tag=$IMAGE_TAG -f k8s/values.yaml --version "${DEPLOY_TAG}" pennlabs/icarus
