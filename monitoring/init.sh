4db1adb7a6dd73e583e41138e377c2e7fb27995d
Peyton Walters <pwalters18@bloomberg.net>
2020-01-19 10:57:33 -0500

    Got logging working

--- monitoring/init.sh
+++ monitoring/init.sh
@@ -4,6 +4,6 @@ kubectl create ns monitoring
 helm upgrade --install prometheus --atomic -f prometheus/values.yaml --namespace monitoring stable/prometheus
 kubectl apply -f grafana/config.yaml
 helm upgrade --install --atomic grafana -f grafana/values.yaml stable/grafana
-# helm upgrade --install fluentd stable/fluentd-elasticsearch -n monitoring -f logging/fluentd.yaml
-# helm upgrade --install elasticsearch elastic/elasticsearch -n monitoring
-# helm upgrade --install kibana elastic/kibana -f logging/kibana.yaml -n monitoring
+helm upgrade --install fluentd stable/fluentd-elasticsearch -n monitoring -f logging/fluentd.yaml
+helm upgrade --install elasticsearch elastic/elasticsearch -n monitoring -f logging/elastic.yaml
+helm upgrade --install kibana elastic/kibana -f logging/kibana.yaml -n monitoring
