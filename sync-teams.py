ec3c3c8652c89159c67707f84919eaabeaa08fff
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2020-01-31 23:17:26 -0500

    Lint

--- sync-teams.py
+++ sync-teams.py
@@ -1,23 +1,22 @@
-from github import Github
-from getpass import getpass
-import re
-import hvac
 import os
+import re
+
+import hvac
+from github import Github
 from jinja2 import Template
+
 
 g = Github(os.getenv("GITHUB_TOKEN"))
 
 teams = []
 for team in g.get_organization("pennlabs").get_teams():
-    m = re.search(r'(\S+)-leads', team.slug)
+    m = re.search(r"(\S+)-leads", team.slug)
     if m:
         teams.append((team.slug, m.group(1)))
 
-client = hvac.Client(
-    url=os.getenv("VAULT_ADDR")
-)
+client = hvac.Client(url=os.getenv("VAULT_ADDR"))
 
-with open('/var/run/secrets/kubernetes.io/serviceaccount/token') as f:
+with open("/var/run/secrets/kubernetes.io/serviceaccount/token") as f:
     jwt = f.read()
     client.auth_kubernetes("team-auth", jwt)
 
@@ -27,13 +26,7 @@ if not client.sys.is_sealed():
         for team in teams:
             team_name, team_slug = team
             pol = t.render(team_name=team_slug)
-            client.sys.create_or_update_policy(
-                name=team_slug,
-                policy=pol,
-            )
-            client.auth.github.map_team(
-                team_name=team_name,
-                policies=[team_slug]
-            )
+            client.sys.create_or_update_policy(name=team_slug, policy=pol)
+            client.auth.github.map_team(team_name=team_name, policies=[team_slug])
 else:
     print("Vault sealed. Stopping.")
