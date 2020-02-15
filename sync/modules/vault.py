864f4c87ba82f4f44445b9e7098e780b5ffb9b4d
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-02-15 12:15:44 -0500

    Vault Policies by Repo (#4)

--- sync/modules/vault.py
+++ sync/modules/vault.py
@@ -6,17 +6,22 @@ from jinja2 import Template
 
 def sync(teams):
     client = hvac.Client(url=os.getenv("VAULT_ADDR"))
-    with open("/var/run/secrets/kubernetes.io/serviceaccount/token") as f:
-        jwt = f.read()
-        client.auth_kubernetes("team-auth", jwt)
+    try:
+        with open("/var/run/secrets/kubernetes.io/serviceaccount/token") as f:
+            jwt = f.read()
+            client.auth_kubernetes("team-auth", jwt)
+    except FileNotFoundError:
+        print("vault: Could access service account token. Exiting.")
+        return
 
     if not client.sys.is_sealed():
         with open("sync/modules/user-policy.hcl.j2") as f:
             t = Template(f.read())
             for team in teams["leads"]:
                 base_team_slug = team.slug.replace("-leads", "")
-                pol = t.render(team_name=base_team_slug)
+                repos = [x.name for x in team.get_repos()]
+                pol = t.render(team_name=base_team_slug, repos=repos)
                 client.sys.create_or_update_policy(name=base_team_slug, policy=pol)
                 client.auth.github.map_team(team_name=team.slug, policies=[base_team_slug])
     else:
-        print("Vault sealed. Stopping.")
+        print("vault: Vault sealed. Stopping.")
