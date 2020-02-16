12244a84fb630275968ae03335243a517ab5d223
William Goeller <william@williamgoeller.com>
2020-02-16 17:39:13 -0500

    Updating the minecraft helm chart to point at our repo

--- labscraft/init.sh
+++ labscraft/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm upgrade --set minecraftServer.eula=true -f values.yaml labscraft stable/minecraft
+helm upgrade --set minecraftServer.eula=true -f values.yaml labscraft pennlabs/labscraft
