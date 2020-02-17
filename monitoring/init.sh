26e7cb83f862a62156ed56652c0a2afed990d23d
Peyton Walters <pwalters18@bloomberg.net>
2020-02-17 16:25:35 -0500

    add timber

--- monitoring/init.sh
+++ monitoring/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Timber also requires secrets. Right now, they exist in vault and are manually copied to monitoring, but once I add
+# cronjobs to icarus, we'll just sync to all namespaces.
+
 kubectl create ns monitoring
 helm upgrade --install prometheus --atomic -f prometheus/values.yaml --namespace monitoring stable/prometheus
 kubectl apply -f grafana/config.yaml
 helm upgrade --install --atomic grafana -f grafana/values.yaml stable/grafana
-helm upgrade --install fluentd stable/fluentd-elasticsearch -n monitoring -f logging/fluentd.yaml
-helm upgrade --install elasticsearch elastic/elasticsearch -n monitoring -f logging/elastic.yaml
-helm upgrade --install kibana elastic/kibana -f logging/kibana.yaml -n monitoring
+kubectl apply -f timber
+
