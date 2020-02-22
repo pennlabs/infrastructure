1782da4babf947aad6389a30344b9a68fe385939
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-02-22 16:23:12 -0500

    Add mysql providers

--- tf/provider.tf
+++ tf/provider.tf
@@ -7,6 +7,21 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "mysql" {
+  version = "~> 1.9"
+  endpoint = "${digitalocean_database_cluster.mysql-production.host}:${digitalocean_database_cluster.mysql-production.port}"
+  username = digitalocean_database_cluster.mysql-production.user
+  password = digitalocean_database_cluster.mysql-production.password
+}
+
+provider "mysql" {
+  alias = "infra"
+  version = "~> 1.9"
+  endpoint = "${digitalocean_database_cluster.mysql-infra.host}:${digitalocean_database_cluster.mysql-infra.port}"
+  username = digitalocean_database_cluster.mysql-infra.user
+  password = digitalocean_database_cluster.mysql-infra.password
+}
+
 terraform {
   backend "s3" {
     bucket         = "labs-tfstate"
