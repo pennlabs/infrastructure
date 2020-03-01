f8a09ae3e6514cdc72baec891d2f4111b5452649
William Goeller <william@williamgoeller.com>
2020-03-01 12:47:24 -0500

    Setup fluentd connection to Elastic Cloud

--- monitoring/init.sh
+++ monitoring/init.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 kubectl create ns monitoring
-helm upgrade --install prometheus --atomic -f prometheus/values.yaml --namespace monitoring stable/prometheus
+helm upgrade --install prometheus --atomic -f prometheus/values.yaml -n monitoring stable/prometheus
 kubectl apply -f grafana/config.yaml
 helm upgrade --install --atomic grafana -f grafana/values.yaml stable/grafana
-helm upgrade --install fluentd stable/fluentd-elasticsearch -n monitoring -f logging/fluentd.yaml
-helm upgrade --install elasticsearch elastic/elasticsearch -n monitoring -f logging/elastic.yaml
-helm upgrade --install kibana elastic/kibana -f logging/kibana.yaml -n monitoring
+helm upgrade --install fluentd kiwigrid/fluentd-elasticsearch -n monitoring -f logging/fluentd.yaml
