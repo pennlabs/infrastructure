9d3f25f064a85f374d61eace395c5b12aff3d374
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-03-02 16:03:14 -0500

    Update cluster module

--- tf/modules/cluster/main.tf
+++ tf/modules/cluster/main.tf
@@ -1,13 +1,11 @@
 resource "digitalocean_kubernetes_cluster" "cluster" {
-  name    = "${var.name}"
+  name    = var.name
   region  = "nyc1"
-
-  // Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.16.2-do.0"
+  version = var.cluster_version
 
   node_pool {
-    name       = "${var.name}"
-    size       = "${var.node_size}"
-    node_count = var.node_count 
+    name       = var.name
+    size       = var.node_size
+    node_count = var.node_count
   }
 }
--- tf/modules/cluster/variables.tf
+++ tf/modules/cluster/variables.tf
@@ -3,6 +3,12 @@ variable "name" {
   type        = string
 }
 
+variable "cluster_version" {
+  description = "Kubernetes version of the cluster"
+  type        = string
+  default     = "1.16.6-do.0"
+}
+
 variable "node_count" {
   description = "Number of nodes in cluster"
   type        = number
@@ -11,5 +17,5 @@ variable "node_count" {
 variable "node_size" {
   description = "DigitalOcean size for nodes"
   type        = string
-  default = "s-2vcpu-4gb"
+  default     = "s-2vcpu-4gb"
 }
