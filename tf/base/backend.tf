e4e1b72b407f54da9ad98ceee46c3a8af6953738
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-03-02 15:52:28 -0500

    Add postvault backend

--- tf/base/backend.tf
+++ tf/base/backend.tf
@@ -3,3 +3,9 @@ module "base_tfstate_backend" {
   name   = "base-tfstate"
   region = "us-east-1"
 }
+
+module "postvault_tfstate_backend" {
+  source = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.13.0"
+  name   = "postvault-tfstate"
+  region = "us-east-1"
+}
