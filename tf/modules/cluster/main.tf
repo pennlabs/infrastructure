7c027e3391cc7986de5bb7d34f76d8d03f9f255e
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-03-03 16:42:39 -0500

    Provision infrastructure db in cluster module

--- tf/modules/cluster/main.tf
+++ tf/modules/cluster/main.tf
@@ -1,11 +1,21 @@
 resource "digitalocean_kubernetes_cluster" "cluster" {
   name    = var.name
   region  = "nyc1"
-  version = var.cluster_version
+  version = var.k8s_cluster_version
 
   node_pool {
     name       = var.name
-    size       = var.node_size
-    node_count = var.node_count
+    size       = var.k8s_node_size
+    node_count = var.k8s_node_count
   }
+}
+
+module "infrastructure-database" {
+  source          = "../mysql_cluster"
+  name            = "mysql-${var.name}-infrastructure"
+  node_count      = var.mysql_node_count
+  node_size       = var.mysql_node_size
+  cluster_version = var.mysql_cluster_version
+  users           = var.mysql_users
+  pgp_key         = var.pgp_key
 }
--- tf/modules/cluster/variables.tf
+++ tf/modules/cluster/variables.tf
@@ -3,19 +3,48 @@ variable "name" {
   type        = string
 }
 
-variable "cluster_version" {
+// Kubernetes cluster inputs
+variable "k8s_cluster_version" {
   description = "Kubernetes version of the cluster"
   type        = string
   default     = "1.16.6-do.0"
 }
 
-variable "node_count" {
+variable "k8s_node_count" {
   description = "Number of nodes in cluster"
   type        = number
 }
 
-variable "node_size" {
+variable "k8s_node_size" {
   description = "DigitalOcean size for nodes"
   type        = string
   default     = "s-2vcpu-4gb"
+}
+
+// MySQL database inputs
+variable "mysql_cluster_version" {
+  description = "MySQL version of the cluster"
+  type        = number
+  default     = 8
+}
+
+variable "mysql_node_count" {
+  description = "Number of nodes in MySQL cluster"
+  type        = number
+}
+
+variable "mysql_node_size" {
+  description = "DigitalOcean size for MySQL nodes"
+  type        = string
+  default     = "db-s-1vcpu-1gb"
+}
+
+variable "mysql_users" {
+  description = "List of names to generate DBs and users from"
+  type        = set(string)
+}
+
+variable "pgp_key" {
+  description = "PGP key to encrypt passwords with"
+  type        = string
 }
