e2c4f366b158b73f0b9c994eb18c8e43cf396b37
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-05-12 20:56:36 -0400

    Configure base cluster tf module

---
+++ tf/modules/base_cluster/main.tf
@@ -0,0 +1,46 @@
+// K8s Cluster
+resource "digitalocean_kubernetes_cluster" "cluster" {
+  name    = var.name
+  region  = "nyc1"
+  version = var.k8s_cluster_version
+
+  node_pool {
+    name       = var.name
+    size       = var.k8s_node_size
+    node_count = var.k8s_node_count
+  }
+}
+
+// Traefik
+resource "helm_release" "traefik" {
+  name       = "traefik"
+  repository = "https://kubernetes-charts.storage.googleapis.com"
+  chart      = "traefik"
+  version    = "1.86.2"
+  namespace  = "kube-system"
+
+  values = var.traefik_values
+}
+
+// Cert Manager
+resource "kubernetes_namespace" "cert-manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "0.14.0"
+  namespace  = kubernetes_namespace.cert-manager.metadata[0].name
+
+  values = var.cert_manager_values
+
+  // TODO: this isn't working
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
---
+++ tf/modules/base_cluster/variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  description = "Name for cluster"
+  type        = string
+}
+
+// Kubernetes cluster inputs
+variable "k8s_cluster_version" {
+  description = "Kubernetes version of the cluster"
+  type        = string
+  default     = "1.17.5-do.0"
+}
+
+variable "k8s_node_count" {
+  description = "Number of nodes in cluster"
+  type        = number
+}
+
+variable "k8s_node_size" {
+  description = "DigitalOcean size for nodes"
+  type        = string
+  default     = "s-2vcpu-4gb"
+}
+
+// Traefik values
+variable "traefik_values" {
+  description = "Values to provide to Traefik"
+  type        = list(string)
+}
+
+// Cert Manager values
+variable "cert_manager_values" {
+  description = "Values to provide to Cert Manager"
+  type        = list(string)
+}
