53fb36c211ce7bc40a8a8781e176a5bfbc0e871b
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-05-15 16:00:18 -0400

    base tf s3 bucket

--- tf/chronos/backends.tf
+++ tf/chronos/backends.tf
@@ -10,10 +10,8 @@ module "vault_tfstate_backend" {
   region = "us-east-1"
 }
 
-// module "base_tfstate_backend" {
-//   source = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.17.0"
-//   name   = "base-tfstate"
-//   region = "us-east-1"
-// }
-
-
+module "base_tfstate_backend" {
+  source = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.17.0"
+  name   = "base-tfstate"
+  region = "us-east-1"
+}
