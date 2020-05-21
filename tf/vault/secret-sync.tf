c909e79b4aca8342e49100b09eb4844a0f280687
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-05-21 15:16:24 -0400

    Sync secrets to more namespaces

--- tf/vault/secret-sync.tf
+++ tf/vault/secret-sync.tf
@@ -15,10 +15,22 @@ resource "vault_approle_auth_backend_role_secret_id" "secret-sync" {
   role_name = vault_approle_auth_backend_role.secret-sync.role_name
 }
 
-// TODO: will likely need to add the staging NS to prod
-resource "vault_generic_secret" "secret-sync" {
+// TODO: do we need to sync to more namespaces?
+resource "vault_generic_secret" "secret-sync-default" {
   for_each = toset(["chronos", "sandbox", "production"])
   path     = "${vault_mount.secrets.path}/${each.key}/default/secret-sync"
+
+  data_json = <<EOT
+{
+  "ROLE_ID":   "${vault_approle_auth_backend_role.secret-sync.role_id}",
+  "SECRET_ID": "${vault_approle_auth_backend_role_secret_id.secret-sync.secret_id}"
+}
+EOT
+}
+
+resource "vault_generic_secret" "secret-sync-monitoring" {
+  for_each = toset(["chronos", "sandbox", "production"])
+  path     = "${vault_mount.secrets.path}/${each.key}/monitoring/secret-sync"
 
   data_json = <<EOT
 {
