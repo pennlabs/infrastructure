08cd4d385693b3d113d3f3c3ff2cefe766abb006
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-05-21 17:28:18 -0400

    bump team-sync version

--- tf/chronos/team-sync.tf
+++ tf/chronos/team-sync.tf
@@ -2,7 +2,7 @@ resource "helm_release" "team-sync" {
   name       = "team-sync"
   repository = "https://helm.pennlabs.org"
   chart      = "icarus"
-  version    = "0.1.16"
+  version    = "0.1.17"
 
   values = [
     file("helm/team-sync.yaml")
