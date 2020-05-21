56b48b925fa558cb50d64f434baac8bdb02a9ffb
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-05-21 10:33:39 -0400

    Update vault secret sync version

--- tf/modules/base_cluster/vault-secret-sync.tf
+++ tf/modules/base_cluster/vault-secret-sync.tf
@@ -2,6 +2,6 @@ resource "helm_release" "vault-secret-sync" {
   name       = "vault-secret-sync"
   repository = "https://helm.pennlabs.org"
   chart      = "vault-secret-sync"
-  version    = "0.1.0"
+  version    = "0.1.2"
   values = var.vault_secret_sync_values
 }
