c69ee1a4cf693eb762840b2a6424c0279fe7656e
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-05-22 19:33:37 -0400

    Delete old terraform configuration

--- tf/old/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "KUBECONFIG" {
-  sensitive   = true
-  value = digitalocean_kubernetes_cluster.labs-prod.kube_config[0].raw_config
-}
-
-output "JUMP_IP" {
-  sensitive   = true
-  value = digitalocean_droplet.jump.ipv4_address
-}
