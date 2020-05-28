8c5b82660c5cc1f8348e75e4a2ec19bf00086e3a
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-05-27 23:24:01 -0400

    Fix fluentd

--- terraform/modules/base_cluster/monitoring.tf
+++ terraform/modules/base_cluster/monitoring.tf
@@ -18,7 +18,7 @@ resource "helm_release" "fluentd" {
   name       = "fluentd"
   repository = "https://kiwigrid.github.io"
   chart      = "fluentd-elasticsearch"
-  version    = "9.1.1"
+  version    = "9.2.0"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
   values = var.fluentd_values
