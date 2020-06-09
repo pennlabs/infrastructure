ba005f745459c05e1841ac1e1df3086ca0754e48
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-06-09 15:19:34 -0400

    Bump icarus

--- terraform/base/bitwarden.tf
+++ terraform/base/bitwarden.tf
@@ -2,7 +2,7 @@ resource "helm_release" "bitwarden" {
   name       = "bitwarden"
   repository = "https://helm.pennlabs.org"
   chart      = "icarus"
-  version    = "0.1.17"
+  version    = "0.1.18"
 
   values = [
     "${file("helm-production/bitwarden.yaml")}"
--- terraform/chronos/team-sync.tf
+++ terraform/chronos/team-sync.tf
@@ -2,7 +2,7 @@ resource "helm_release" "team-sync" {
   name       = "team-sync"
   repository = "https://helm.pennlabs.org"
   chart      = "icarus"
-  version    = "0.1.17"
+  version    = "0.1.18"
 
   values = [
     file("helm/team-sync.yaml")
