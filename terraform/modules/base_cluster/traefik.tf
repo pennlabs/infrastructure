86b54f7bad786eeacee1f6f6ab5d2ff1c499d092
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-08-05 14:38:33 -0400

    Better prometheus support

--- terraform/modules/base_cluster/traefik.tf
+++ terraform/modules/base_cluster/traefik.tf
@@ -2,7 +2,7 @@ resource "helm_release" "traefik" {
   name       = "traefik"
   repository = "https://kubernetes-charts.storage.googleapis.com"
   chart      = "traefik"
-  version    = "1.86.2"
+  version    = "1.87.2"
   namespace  = "kube-system"
 
   values = var.traefik_values
