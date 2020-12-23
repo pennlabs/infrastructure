37116719080f7c150341d68181c07728d577ed3f
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-12-23 01:09:06 -0500

    Temporarily build on feature branch

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -2,5 +2,6 @@ import { App } from "cdkactions";
 import { CDKStack } from "@pennlabs/kraken"
 
 const app = new App();
-new CDKStack(app, 'kraken');
+// TODO: remove modified default branch
+new CDKStack(app, 'kraken', { defaultBranch: 'feature/kraken' });
 app.synth();
