58ac07046811ca3c3e45cd2554ef7a3ef92a1df5
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-12-29 01:59:01 -0500

    Remove django stack test

--- cdk/kraken/test/application.test.ts
+++ cdk/kraken/test/application.test.ts
@@ -1,22 +1,7 @@
 import * as fs from 'fs';
-import { DjangoStack, ApplicationStack } from '../src';
+import { ApplicationStack } from '../src';
 import { TestingApp } from './utils';
 
-
-// DjangoStack
-test('default', () => {
-  const app = TestingApp({ createValidateWorkflow: false });
-  new DjangoStack(app, {
-    djangoProjectName: 'example',
-    dockerImageName: 'example',
-  });
-  app.synth();
-  expect(fs.readdirSync(app.outdir)).toEqual([
-    'cdkactions_build-and-deploy.yaml',
-  ]);
-  expect(fs.readFileSync(`${app.outdir}/cdkactions_build-and-deploy.yaml`, 'utf-8')).toMatchSnapshot();
-},
-);
 
 // ApplicationStack
 test('default', () => {
