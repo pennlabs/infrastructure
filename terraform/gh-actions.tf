b1d0f5784961eee15e7bc6efbca68b67d1ae0ac9
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-01-11 17:41:11 -0500

    Allow gh actions user to describe cluster

--- terraform/gh-actions.tf
+++ terraform/gh-actions.tf
@@ -10,8 +10,14 @@ resource "aws_iam_access_key" "gh-actions" {
   user = aws_iam_user.gh-actions.name
 }
 
-resource "aws_iam_user_policy" "gh-actions" {
-  name   = "gh-actions"
+resource "aws_iam_user_policy" "gh-actions-assume-kubectl" {
+  name   = "kubectl"
   user   = aws_iam_user.gh-actions.name
   policy = data.aws_iam_policy_document.assume-kubectl.json
+}
+
+resource "aws_iam_user_policy" "gh-actions-view-k8s" {
+  name   = "view-eks"
+  user   = aws_iam_user.gh-actions.name
+  policy = data.aws_iam_policy_document.view-k8s.json
 }
