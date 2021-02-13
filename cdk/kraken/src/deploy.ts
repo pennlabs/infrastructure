b209013f5fd921bf4ca89aeee8f358d00f386941
Peyton Walters <pawalt@hey.com>
2021-02-13 11:21:05 -0500

    Propogate deploy failures

--- cdk/kraken/src/deploy.ts
+++ cdk/kraken/src/deploy.ts
@@ -52,10 +52,13 @@ export class DeployJob extends CheckoutJob {
       if: `github.ref == 'refs/heads/${fullConfig.defaultBranch}'`,
       steps: [{
         name: 'Deploy',
-        run: dedent`curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DO_AUTH_TOKEN" "https://api.digitalocean.com/v2/kubernetes/clusters/\${K8S_CLUSTER_ID}/kubeconfig" > /kubeconfig.conf
-        
+        run: dedent`# default options to make sure failures stop script execution
+        set -euo pipefail
+
+        curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DO_AUTH_TOKEN" "https://api.digitalocean.com/v2/kubernetes/clusters/\${K8S_CLUSTER_ID}/kubeconfig" > /kubeconfig.conf
+
         export KUBECONFIG=/kubeconfig.conf
-        
+
         # get repo name (by removing owner/organization)
         RELEASE_NAME=\${REPOSITORY#*/}
 
@@ -65,14 +68,21 @@ export class DeployJob extends CheckoutJob {
             echo "Could not find deploy tag"
             exit 1
         fi
-        
+
         helm repo add pennlabs https://helm.pennlabs.org/
-        
         for i in {1..10}; do
-          helm upgrade --install --atomic --set=image_tag=$IMAGE_TAG -f k8s/values.yaml --version "\${DEPLOY_TAG}" $RELEASE_NAME pennlabs/icarus && exit 0
+          # This is bash soup, but it'll do.
+          # 1. Attempt to install with helm
+          # 2. If this suceeds, exit with a success status code
+          # 3. If it fails, mark the command as suceeded so that `-e` doesn't kick us out
+          # 4. Wait 10s and try again
+          helm upgrade --install --atomic --set=image_tag=$IMAGE_TAG -f k8s/values.yaml --version "\${DEPLOY_TAG}" $RELEASE_NAME pennlabs/icarus && exit 0 || true
           sleep 10s
           echo "Retrying deploy for $i times"
-        done`,
+        done
+
+        # If we get here, all helm installs failed so our command should fail
+        exit 1`,
         env: {
           IMAGE_TAG: fullConfig.deployTag,
           DO_AUTH_TOKEN: '${{ secrets.DO_AUTH_TOKEN }}',
