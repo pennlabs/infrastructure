0cbcceb59f33648e015987a4eb09fedf58945458
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-03-24 20:57:48 -0400

    Sync leads

---
+++ docker/team-sync/sync/modules/platform.py
@@ -0,0 +1,43 @@
+import os
+
+import requests
+
+
+PLATFORM_API_KEY = os.environ.get("PLATFORM_API_KEY", "")
+
+
+def sync(teams, pennkeys):
+    """
+    Sync team leads from GitHub to admin permissions on platform
+    """
+    # Dictionary of pennkey to list of admin permissions
+    content = {}
+    team_slugs = []
+
+    # Team Leads are admins of their products
+    for team in teams["leads"]:
+        team_slug = team.slug[:-6]  # Trim -leads
+        slug = f"{team_slug.replace('-', '_')}_admin"
+        team_slugs.append(slug)
+        for member in team.get_members():
+            gh_username = member.login.lower()
+            pennkey = pennkeys.get(gh_username, None)
+            if pennkey:
+                content.setdefault(pennkey, []).append(slug)
+                # If user is a lead of clubs, also make h@p admin
+                if slug == "penn_clubs_admin":
+                    content[pennkey].append("hub_admin")
+
+    # Directors are admins on all products
+    for team in teams["directors"]:
+        for member in team.get_members():
+            gh_username = member.login.lower()
+            pennkey = pennkeys.get(gh_username, None)
+            if pennkey:
+                content[pennkey] = team_slugs
+
+    headers = {"Authorization": f"Api-Key {PLATFORM_API_KEY}"}
+    requests.post(
+        "https://platform.pennlabs.org/accounts/productadmin/", json=content, headers=headers
+    )
+    return
--- docker/team-sync/sync/sync.py
+++ docker/team-sync/sync/sync.py
@@ -2,7 +2,30 @@
 import os
 import pkgutil
 
+from airtable import Airtable
 from github import Github
+
+
+AIRTABLE_BASE_KEY = os.environ.get("AIRTABLE_BASE_KEY")
+AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
+airtable = Airtable(AIRTABLE_BASE_KEY, "Roster", AIRTABLE_API_KEY)
+
+
+def map_gh_pennkeys():
+    """
+    Create a dictionary of GitHub usernames to PennKeys.
+    """
+    roster = airtable.get_all(view="Platform Sync", fields=["Github", "PennKey"])
+    pennkeys = {}
+    for record in roster:
+        fields = record["fields"]
+        if "Github" in fields and "PennKey" in fields:
+            github_url = fields["Github"].lower()
+            pennkey = fields["PennKey"].lower()
+            if github_url.startswith("https://github.com/"):
+                github_id = github_url.split("/")[3]
+                pennkeys[github_id] = pennkey
+    return pennkeys
 
 
 def run():
@@ -20,12 +43,15 @@ def run():
         else:  # Found a Regular Team
             teams.setdefault("members", []).append(team)
 
+    # Generate dictionary of GitHub usernames to PennKey
+    pennkeys = map_gh_pennkeys()
+
     # Dynamically find each module and run its sync method
     location = os.path.join(os.path.dirname(os.path.abspath(__file__)), "modules")
     for finder, name, _ in pkgutil.walk_packages([location]):
         try:
             module = finder.find_module(name).load_module(name)
-            module.sync(teams)
+            module.sync(teams, pennkeys)
         except (AttributeError, TypeError) as e:
             print(f"Could not execute module '{name}'. The following exception occurred: {e}")
 
