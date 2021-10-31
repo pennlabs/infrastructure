9ecca84557689b8d407b358a73bcba71c9bf1727
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-10-31 12:16:21 -0400

    Update certificate

--- cdk/kittyhawk/src/certificate.ts
+++ cdk/kittyhawk/src/certificate.ts
@@ -1,34 +1,29 @@
 import { Construct } from 'constructs';
 import { Certificate as CertApiObject } from './imports/cert-manager.io';
-import { IngressProps, domainToCertName, removeSubdomain } from './ingress';
+import { domainToCertName, removeSubdomain, HostRules } from './ingress';
 
 export class Certificate extends Construct {
-  constructor(scope: Construct, appname: string, props: IngressProps) {
+  constructor(scope: Construct, appname: string, rules: HostRules[]) {
     super(scope, `certificate-${appname}`);
 
-    // Only generate certificates if an ingress is defined
-    if (props.ingress) {
-      // We want to generate a certificate for each host
-      for (const h of props.ingress) {
-        const hostString: string = domainToCertName(h.host, h.isSubdomain);
-        const finalDomain: string = removeSubdomain(h.host, h.isSubdomain);
-        new CertApiObject(this, `certificate-${appname}-${hostString}`, {
-          metadata: {
-            name: hostString,
+    // We want to generate a certificate for each host
+    rules.map(h => {
+      const hostString: string = domainToCertName(h.host, h.isSubdomain);
+      const finalDomain: string = removeSubdomain(h.host, h.isSubdomain);
+      new CertApiObject(this, `certificate-${appname}-${hostString}`, {
+        metadata: {
+          name: hostString,
+        },
+        spec: {
+          secretName: `${hostString}-tls`,
+          dnsNames: [`${finalDomain}`, `*.${finalDomain}`],
+          issuerRef: {
+            name: 'wildcard-letsencrypt-prod',
+            kind: 'ClusterIssuer',
+            group: 'cert-manager.io',
           },
-          spec: {
-            secretName: hostString.concat('-tls'),
-            dnsNames: [`${finalDomain}`, `*.${finalDomain}`],
-            issuerRef: {
-              name: 'wildcard-letsencrypt-prod',
-              kind: 'ClusterIssuer',
-              group: 'cert-manager.io',
-            },
-          },
-        });
-      }
-    } else {
-      throw new Error('Cannot generate certificate if props.ingress is undefined.');
-    }
+        },
+      });
+    });
   }
 }
