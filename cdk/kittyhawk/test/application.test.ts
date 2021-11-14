9a8d23ad894a9e901a881a2ad636c22d8d2c168e
Qijia "Joy" Liu <joyliu.q@gmail.com>
2021-11-14 13:42:51 -0500

    :bug: Fix typo in application tests

--- cdk/kittyhawk/test/application.test.ts
+++ cdk/kittyhawk/test/application.test.ts
@@ -1,10 +1,8 @@
 import { Construct } from 'constructs';
-import { Application, DjangoApplication, ReactApplication, insertIfNotPresent } from '../src/application';
+import { Application, DjangoApplication, ReactApplication, RedisApplication, insertIfNotPresent } from '../src/application';
 import { chartTest, failingTest } from './utils';
 
-
 export function buildTagOverrideChart(scope: Construct) {
-
   /** Overrides the image tag set as env var **/
   new Application(scope, 'serve', {
     deployment: {
@@ -14,44 +12,108 @@ export function buildTagOverrideChart(scope: Construct) {
   });
 }
 
-
-export function buildFailingDjangoChart(scope: Construct) {
-
-  /** Django Duplicated DOMAIN Env should fail **/
-  new DjangoApplication(scope, 'platform', {
-    deployment: {
-      image: 'pennlabs/platform',
-      env: [{ name: 'DOMAIN', value: 'platform.pennlabs.org' }],
-    },
-    domains: [{ host: 'platform.pennlabs.org', isSubdomain: true }],
-    djangoSettingsModule: 'Platform.settings.production',
-    ingressPaths: ['/'],
-  });
+function buildRedisChart(scope: Construct) {
+  new RedisApplication(scope, 'redis', {});
 }
-
-export function buildFailingReactChart(scope: Construct) {
-
-  /** React Duplicated DOMAIN Env should fail **/
-  new ReactApplication(scope, 'react', {
-    deployment: {
-      image: 'pennlabs/penn-clubs-frontend',
-      replicas: 2,
-      env: [{ name: 'DOMAIN', value: 'pennclubs.com' }],
-    },
-    domain: 'pennclubs.com',
-    ingressPaths: ['/'],
-    portEnv: '80',
-  });
+function buildFailingDjangoChart(scope: Construct) {
+  new DjangoApplication(scope, 'platform', testConfig.django.failing);
+}
+function buildExampleDjangoChart(scope: Construct) {
+  new DjangoApplication(scope, 'platform', testConfig.django.example);
+}
+function buildDefaultDjangoChart(scope: Construct) {
+  new DjangoApplication(scope, 'platform', testConfig.django.default);
+}
+function buildExampleReactChart(scope: Construct) {
+  new ReactApplication(scope, 'react', testConfig.react.example);
+}
+function buildFailingReactChart(scope: Construct) {
+  new ReactApplication(scope, 'react', testConfig.react.failing);
+}
+function buildDefaultValuesReactChart(scope: Construct) {
+  new ReactApplication(scope, 'react', testConfig.react.default);
 }
 
 test('Tag Override', () => chartTest(buildTagOverrideChart));
 
+test('Redis Application', () => chartTest(buildRedisChart));
 test('Django Application -- Failing', () => failingTest(buildFailingDjangoChart));
-
+test('Django Application -- Example', () => chartTest(buildExampleDjangoChart));
+test('Django Application -- Default', () => chartTest(buildDefaultDjangoChart));
 test('React Application -- Failing', () => failingTest(buildFailingReactChart));
+test('React Application -- Example', () => chartTest(buildExampleReactChart));
+// TODO - add comparison to make sure the default values are correct
+// e.g. (portEnv is assumed to be 80 & replicas is also defaulted)
+test('React Application -- Default', () => chartTest(buildDefaultValuesReactChart));
+
 
 test('insertIfNotPresent throws if already present', () => {
   let myEnvArray = [{ name: 'KEY1', value: 'VALUE1' }];
   expect(() => insertIfNotPresent(myEnvArray, 'KEY1', 'VALUE2'))
-    .toThrow('KEY1 should not be redefined as an enviroment variable.');
+    .toThrow('KEY1 should not be redefined as an environment variable.');
 });
+
+
+const testConfig = {
+  django: {
+    example: {
+      deployment: {
+        image: 'pennlabs/platform',
+        replicas: 2,
+        env: [{ name: 'SOME_ENV', value: 'environment variables are cool' }],
+      },
+      domains: [{ host: 'platform.pennlabs.org', isSubdomain: true }],
+      djangoSettingsModule: 'Platform.settings.production',
+      ingressPaths: ['/'],
+      portEnv: '80',
+    },
+    default: {
+      deployment: {
+        image: 'pennlabs/platform',
+      },
+      domains: [{ host: 'platform.pennlabs.org', isSubdomain: true }],
+      djangoSettingsModule: 'Platform.settings.production',
+      ingressPaths: ['/'],
+    },
+    failing: {
+      deployment: {
+        image: 'pennlabs/platform',
+        /** Django Duplicated DOMAIN Env should fail **/
+        env: [{ name: 'DOMAIN', value: 'platform.pennlabs.org' }],
+      },
+      domains: [{ host: 'platform.pennlabs.org', isSubdomain: true }],
+      djangoSettingsModule: 'Platform.settings.production',
+      ingressPaths: ['/'],
+    },
+  },
+  react: {
+    default: {
+      deployment: {
+        image: 'pennlabs/penn-clubs-frontend',
+      },
+      domain: 'pennclubs.com',
+      ingressPaths: ['/'],
+    },
+    example: {
+      deployment: {
+        image: 'pennlabs/penn-clubs-frontend',
+        replicas: 2,
+        env: [{ name: 'SOME_ENV', value: 'environment variables are cool' }],
+      },
+      domain: 'pennclubs.com',
+      ingressPaths: ['/'],
+      portEnv: '80',
+    },
+    failing: {
+      deployment: {
+        image: 'pennlabs/penn-clubs-frontend',
+        replicas: 2,
+        /** React Duplicated DOMAIN Env should fail **/
+        env: [{ name: 'DOMAIN', value: 'pennclubs.com' }],
+      },
+      domain: 'pennclubs.com',
+      ingressPaths: ['/'],
+      portEnv: '80',
+    },
+  }
+}
