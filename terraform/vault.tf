d8248ed2fc1c79ef199293e7e60988349d035daf
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2022-01-23 17:33:07 -0500

    [Terraform] Fix Vault TLS certificate

--- terraform/vault.tf
+++ terraform/vault.tf
@@ -4,6 +4,27 @@ resource "aws_acm_certificate" "vault" {
   validation_method = "DNS"
 }
 
+resource "aws_route53_record" "vault-tls-validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.vault.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = module.domains["pennlabs.org"].zone_id
+}
+
+resource "aws_acm_certificate_validation" "vault" {
+  certificate_arn         = aws_acm_certificate.vault.arn
+  validation_record_fqdns = [for record in aws_route53_record.vault-tls-validation : record.fqdn]
+}
+
 // KMS
 resource "aws_kms_key" "vault" {
   description = "Key to unseal vault"
@@ -217,7 +238,7 @@ resource "aws_lb_listener" "vault" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.vault.arn
+  certificate_arn   = aws_acm_certificate_validation.vault.certificate_arn
 
   default_action {
     type             = "forward"
