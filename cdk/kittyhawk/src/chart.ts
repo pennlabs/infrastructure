b59b28c735a6c4cfbecc0826e5d1361957d93dea
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2022-03-13 16:49:54 -0400

    Modify chart

--- cdk/kittyhawk/src/chart.ts
+++ cdk/kittyhawk/src/chart.ts
@@ -1,19 +1,15 @@
-import { App, Chart as ChartApiObject } from 'cdk8s';
+import { Chart, ChartProps } from 'cdk8s';
 import { Construct } from 'constructs';
 
-export class PennLabsChart extends ChartApiObject {
-  constructor(scope: Construct, name: string, chartBuilder: (scope: Construct) => void) {
-    // grab the release name from env vars
-    super(scope, name, { labels: { 
-      release: (process.env.RELEASE_NAME || 'undefined_release'),
-      "app.kubernetes.io/version": (process.env.GIT_SHA || process.exit(1)),
-    } });
-    chartBuilder(this);
+export class PennLabsChart extends Chart {
+  constructor(scope: Construct, name: string, props?: ChartProps) {
+    super(scope, name, {
+      namespace: props?.namespace,
+      labels: {
+        "release": (process.env.RELEASE_NAME || 'undefined_release'),
+        "app.kubernetes.io/version": (process.env.GIT_SHA || process.exit(1)),
+        ...props?.labels,
+      },
+    });
   }
-}
-
-export function synth(chartBuilder: (scope: Construct) => void) {
-  const app = new App();
-  new PennLabsChart(app, 'kittyhawk', chartBuilder);
-  app.synth();
 }
