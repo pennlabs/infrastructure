5efdd1a0a15cd7641bf7611625cd6dd723e51e97
Joy Liu <joyliu.q@gmail.com>
2022-03-18 17:48:10 -0400

    :art: Yay

--- cdk/kittyhawk/src/container.ts
+++ cdk/kittyhawk/src/container.ts
@@ -179,9 +179,10 @@ export class Container implements ContainerInterface {
 
 const secretVolumeName = (vm: VolumeMount) => {
   const mountString = (a: string) => a.toLowerCase().split("_").join("-");
-  return `${mountString(vm.name)}-${vm.subPath && mountString(vm.subPath)}`;
+  return `${mountString(vm.name)}${
+    vm.subPath && `-${mountString(vm.subPath)}`
+  }`;
 };
-
 export class SecretVolume implements VolumeInterface {
   /**
    * Name of the volume
