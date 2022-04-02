d2f4b7b2614a1b3f53e68cf846018006be05d56d
Jonathan Kao <jkao97@yahoo.com>
2022-04-02 02:09:14 -0400

    feat(terraform): do not use raw namespace string

--- terraform/modules/base_cluster/datadog.tf
+++ terraform/modules/base_cluster/datadog.tf
@@ -2,7 +2,7 @@ resource "helm_release" "datadog" {
   name       = "datadog"
   repository = "https://helm.datadoghq.com"
   chart      = "datadog"
-  namespace  = "monitoring"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
   values = var.datadog_values
 }
