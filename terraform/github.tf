ab0faf4a88a09cf14c39fb0aa248033a8b9b121e
Joy Liu <joyliu.q@gmail.com>
2022-04-11 17:03:24 -0400

    :new: [Terraform] Add Org Secrets To Private Repos

--- terraform/github.tf
+++ terraform/github.tf
@@ -15,3 +15,29 @@ resource "github_actions_organization_secret" "aws_secret_key" {
   visibility      = "all"
   plaintext_value = aws_iam_access_key.gh-actions.secret
 }
+
+// Set github action secrets for private repositories (bc no access to org secrets)
+data "github_repositories" "private_repos" {
+  query = "org:pennlabs is:private"
+}
+
+resource "github_actions_secret" "private_repos_aws_account_id" {
+  for_each = toset(data.github_repositories.private_repos.names)
+  repository = each.key
+  secret_name = "AWS_ACCOUNT_ID"
+  plaintext_value = data.aws_caller_identity.current.account_id
+}
+
+resource "github_actions_secret" "private_repos_aws_access_key" {
+  for_each = toset(data.github_repositories.private_repos.names)
+  repository = each.key
+  secret_name = "GH_AWS_ACCESS_KEY_ID" 
+  plaintext_value = aws_iam_access_key.gh-actions.id
+}
+
+resource "github_actions_secret" "private_repos_aws_secret_key" {
+  for_each = toset(data.github_repositories.private_repos.names)
+  repository = each.key
+  secret_name = "GH_AWS_SECRET_ACCESS_KEY" 
+  plaintext_value = aws_iam_access_key.gh-actions.secret
+}
