1f42ef6369a7fc88db9ed0483bb01e1e3b5e9005
Joy Liu <joyliu.q@gmail.com>
2022-04-30 18:14:06 -0400

    :art: tf fmt

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -9,8 +9,8 @@ module "eks-production" {
   vpc_id          = module.vpc.vpc_id
   eks_managed_node_groups = {
     spot = {
-      max_size     = local.k8s_cluster_size
-      min_size     = local.k8s_cluster_size
+      max_size = local.k8s_cluster_size
+      min_size = local.k8s_cluster_size
 
       create_launch_template = false
       launch_template_name   = ""
