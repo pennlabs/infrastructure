f66f2639e37ecb4dade8add3c77413d8b9962075
Joy Liu <joyliu.q@gmail.com>
2023-09-15 00:00:19 -0400

    :arrow_up: Bump Kittyhawk Version

--- cdk/kittyhawk/.projenrc.js
+++ cdk/kittyhawk/.projenrc.js
@@ -1,26 +1,30 @@
-const { TypeScriptProject } = require('projen/lib/typescript');
-const common = require('../projen-common');
+const { TypeScriptProject } = require("projen/lib/typescript");
+const common = require("../projen-common");
 
 const project = new TypeScriptProject({
-  name: '@pennlabs/kittyhawk',
-  description: 'Tool to generate Kubernetes YAML files using Typescript. Built for Penn Labs.',
-  deps: ['cdk8s', 'constructs', 'cron-time-generator', 'cdk8s-cli'],
-  devDeps: ['codecov'],
-  keywords: ['cdk', 'yaml', 'kubernetes', 'constructs', 'cdk8s'],
-  homepage: 'https://kittyhawk.pennlabs.org',
-  repositoryDirectory: 'cdk/kittyhawk',
+  name: "@pennlabs/kittyhawk",
+  description:
+    "Tool to generate Kubernetes YAML files using Typescript. Built for Penn Labs.",
+  deps: ["cdk8s", "constructs", "cron-time-generator", "cdk8s-cli"],
+  devDeps: ["codecov"],
+  keywords: ["cdk", "yaml", "kubernetes", "constructs", "cdk8s"],
+  homepage: "https://kittyhawk.pennlabs.org",
+  repositoryDirectory: "cdk/kittyhawk",
   ...common.options,
   scripts: {
-    import: 'yarn run cdk8s import --output src/imports',
+    import: "yarn run cdk8s import --output src/imports",
   },
 });
 
-project.addFields({['version']: '1.1.7'});
-project.prettier?.ignoreFile?.addPatterns('src/imports');
-project.testTask.steps.forEach(step => {
+project.addFields({ ["version"]: "1.1.8" });
+project.prettier?.ignoreFile?.addPatterns("src/imports");
+project.testTask.steps.forEach((step) => {
   if (step.exec) {
-    step.exec = step.exec.replace(' --updateSnapshot', '');
+    step.exec = step.exec.replace(" --updateSnapshot", "");
   }
 });
-project.setScript('test', "export GIT_SHA='TESTSHA' AWS_ACCOUNT_ID='TEST_AWS_ACCOUNT_ID' && npx projen test");
+project.setScript(
+  "test",
+  "export GIT_SHA='TESTSHA' AWS_ACCOUNT_ID='TEST_AWS_ACCOUNT_ID' && npx projen test"
+);
 project.synth();
