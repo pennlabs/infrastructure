e276cef56a03a2f0bc03eb88d35597ee4b2dc31c
Joy Liu <joyliu.q@gmail.com>
2023-10-22 10:35:25 -0400

    Snapshott

--- cdk/kittyhawk/test/deployment.test.ts
+++ cdk/kittyhawk/test/deployment.test.ts
@@ -1,11 +1,24 @@
 import { Construct } from "constructs";
 import { Container, Deployment } from "../src";
 import { chartTest, failingTestNoGitSha } from "./utils";
+import { KubeServiceAccount } from "../src/imports/k8s";
 
 export function buildDeploymentDefault(scope: Construct) {
   new Deployment(scope, "container", {
     image: "pennlabs/website",
     tag: "latest",
+  });
+}
+
+export function buildDeploymentWithServiceAccount(scope: Construct) {
+  new Deployment(scope, "container", {
+    image: "pennlabs/website",
+    tag: "latest",
+    serviceAccount: new KubeServiceAccount(scope, "service-account", {
+      metadata: {
+        name: "service-account",
+      },
+    }),
   });
 }
 
@@ -18,4 +31,7 @@ export function buildContainerDefault() {
 
 test("Deployment -- No Git Sha", () =>
   failingTestNoGitSha(buildDeploymentDefault));
+test("Deployment -- With Service Account", () =>
+  chartTest(buildDeploymentWithServiceAccount));
 test("Deployment -- Default", () => chartTest(buildDeploymentDefault));
+test("Container -- Default", () => chartTest(buildContainerDefault));
