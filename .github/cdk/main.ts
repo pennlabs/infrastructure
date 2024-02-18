5de090304212f6f6e103234feb6a973508106eaa
Eunsoo Shin <62971511+esinx@users.noreply.github.com>
2024-02-18 01:00:03 -0500

    Bump 1.11 (#191)
    * version bump
    
    * bump django version
    
    * wow I feel like a platform newbie
    
    * bump(cdkactions)
    
    * chore(cdkactions/validate)

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -1,47 +1,53 @@
-import { App, CheckoutJob, Stack, Workflow } from "cdkactions";
+import { App, CheckoutJob, Stack, Workflow } from "cdkactions"
 import { CDKPublishStack } from "@pennlabs/kraken"
-import { Construct } from "constructs";
-import { DjangoBaseDockerStack, DockerPublishStack, ShibbolethDockerStack } from "./docker";
+import { Construct } from "constructs"
+import {
+	DjangoBaseDockerStack,
+	DockerPublishStack,
+	ShibbolethDockerStack,
+} from "./docker"
 
 class TerraformLintStack extends Stack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
+	constructor(scope: Construct, name: string) {
+		super(scope, name)
 
-    const workflow = new Workflow(this, 'terraform', {
-      name: 'Lint terraform files',
-      on: {
-        push: {
-          paths: ['terraform/**.tf']
-        }
-      },
-    });
+		const workflow = new Workflow(this, "terraform", {
+			name: "Lint terraform files",
+			on: {
+				push: {
+					paths: ["terraform/**.tf"],
+				},
+			},
+		})
 
-    new CheckoutJob(workflow, 'lint', {
-      runsOn: 'ubuntu-latest',
-      steps: [
-        {
-          uses: 'hashicorp/setup-terraform@v1'
-        },
-        {
-          run: 'terraform fmt -check -recursive terraform'
-        }
-      ],
-    });
-  }
+		new CheckoutJob(workflow, "lint", {
+			runsOn: "ubuntu-latest",
+			steps: [
+				{
+					uses: "hashicorp/setup-terraform@v1",
+				},
+				{
+					run: "terraform fmt -check -recursive terraform",
+				},
+			],
+		})
+	}
 }
 
-const app = new App();
+const app = new App()
 
 // CDK stacks
-new CDKPublishStack(app, 'kraken');
-new CDKPublishStack(app, 'kittyhawk');
+new CDKPublishStack(app, "kraken")
+new CDKPublishStack(app, "kittyhawk")
 
 // Docker stacks
-const dockerImages = ['datadog-agent', 'pg-s3-backup', 'team-sync'];
-dockerImages.map(name => new DockerPublishStack(app, name));
-new DjangoBaseDockerStack(app, {pythonVersions: ['3.9.14', '3.8.5', '3.10.1']});
-new ShibbolethDockerStack(app);
+const dockerImages = ["datadog-agent", "pg-s3-backup", "team-sync"]
+dockerImages.map((name) => new DockerPublishStack(app, name))
+new DjangoBaseDockerStack(app, {
+	pythonVersions: ["3.9.14", "3.8.5", "3.10.1", "3.11"],
+})
+new ShibbolethDockerStack(app)
 
 // Misc stacks
-new TerraformLintStack(app, 'terraform');
-app.synth();
+new TerraformLintStack(app, "terraform")
+app.synth()
