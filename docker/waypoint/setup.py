1a1a8460052f31f1a6fe3a19bd04cd9590f8aa8c
Christopher Liu <liuchris@seas.upenn.edu>
2025-08-29 20:24:16 -0400

    Bump version to v1.0.4

--- docker/waypoint/setup.py
+++ docker/waypoint/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="waypoint",
-    version="1.0.3",
+    version="1.0.4",
     package_dir={"": "src"},
     py_modules=["main", "waypoint_client"],
     install_requires=[],
